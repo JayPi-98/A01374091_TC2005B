3b7c775093d131bc428884faf223796182cb8c05
Juan Pablo Moreno <jpmoreno98@gmail.com>
2023-03-29 12:35:45 -0600

    Cambios en el archivo eventos_dom.js

--- Desarrollo Web/Front/JS/eventos_dom.js
+++ Desarrollo Web/Front/JS/eventos_dom.js
@@ -16,52 +16,52 @@ document.getElementById("form1-submit").addEventListener("click", function(event
   });
 
 // Ejercicio 3
-  var table = document.getElementById("sampleTable");
+  let addTable = document.getElementById("sampleTable");
 
   document.getElementById("btn-insert-r").addEventListener("click", function() {
-    var newRow = table.insertRow();
-    var newCell1 = newRow.insertCell();
-    var newCell2 = newRow.insertCell();
+    let newRow = addTable.insertRow();
+    let newCell1 = newRow.insertCell();
+    let newCell2 = newRow.insertCell();
     newCell1.innerHTML = "New row column 1";
     newCell2.innerHTML = "New row column 2";
   });
   
   document.getElementById("btn-insert-c").addEventListener("click", function() {
-    var rows = table.rows;
-    for (var i = 0; i < rows.length; i++) {
-      var newCell = rows[i].insertCell();
+    let rows = addTable.rows;
+    for (let i = 0; i < rows.length; i++) {
+      let newCell = rows[i].insertCell();
       newCell.innerHTML = "New column";
     }
   });
 
 // Ejercicio 4
-var table = document.getElementById("myTable");
+let inTable = document.getElementById("myTable");
 
 document.getElementById("btn-change").addEventListener("click", function() {
-  var rowIndex = parseInt(document.getElementById("rowIndex").value);
-  var colIndex = parseInt(document.getElementById("colIndex").value);
-  var newValue = document.getElementById("newValue").value;
+  let rowIndex = parseInt(document.getElementById("rowIndex").value);
+  let colIndex = parseInt(document.getElementById("colIndex").value);
+  let newValue = document.getElementById("newValue").value;
   
-  if (rowIndex < table.rows.length && colIndex < table.rows[rowIndex].cells.length) {
-    table.rows[rowIndex].cells[colIndex].innerHTML = newValue;
+  if (rowIndex < inTable.rows.length && colIndex < inTable.rows[rowIndex].cells.length) {
+    inTable.rows[rowIndex].cells[colIndex].innerHTML = newValue;
   } else {
     alert("Invalid row or column index!");
   }
 });
 
 // Ejercicio 5
-var colorSelect = document.getElementById("colorSelect");
+const colorSelect = document.getElementById("colorSelect");
 
 // Función para agregar un color a la lista de opciones
 document.getElementById("btn-add-color").addEventListener("click", function() {
-  var newColor = document.createElement("option");
+  let newColor = document.createElement("option");
   newColor.text = getRandomColor(); // Genera un color aleatorio
   colorSelect.add(newColor);
 });
 
 // Función para quitar el color seleccionado de la lista de opciones
 document.getElementById("btn-rmv-color").addEventListener("click", function() {
-  var selectedIndex = colorSelect.selectedIndex;
+  let selectedIndex = colorSelect.selectedIndex;
   if (selectedIndex >= 0) {
     colorSelect.remove(selectedIndex);
   }
@@ -69,9 +69,9 @@ document.getElementById("btn-rmv-color").addEventListener("click", function() {
 
 // Función para generar un color aleatorio
 function getRandomColor() {
-  var letters = "0123456789ABCDEF";
-  var color = "#";
-  for (var i = 0; i < 6; i++) {
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
